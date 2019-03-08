5a2d2b381449a2ce63efc6dc41ea78671597731b
Daniel Björkander <danielb@wip.se>
2019-03-08 21:35:37 +0100

    Initial Commit

---
+++ Snart/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Snart
+//
+//  Created by Daniel Björkander on 2019-03-08.
+//  Copyright © 2019 Daniel Björkander. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
